d7b939e2f06198aa6e4a30df951bb431a5acb993
Михаил <89205520029@bk.ru>
2013-06-06 17:13:55 +0400

    Update codemirror.plugin.php
    edit

--- assets/plugins/codemirror/codemirror.plugin.php
+++ assets/plugins/codemirror/codemirror.plugin.php
@@ -3,27 +3,22 @@
  * @name        CodeMirror
  * @description JavaScript library that can be used to create a relatively pleasant editor interface
  *
- * @released    Apr 15, 2012
- * @CodeMirror  2.23 b
+ * @released    Jun 5, 2013
+ * @CodeMirror  3.13
  *
  * @required    MODx 0.9.6.3+
- *              CodeMirror  2.23 : pl
+ *              CodeMirror  3.13 : pl
  *
- * @confirmed   MODx Evolution 1.0.6
+ * @confirmed   MODx Evolution 1.10.0
  *
- * @author      hansek from www.modxcms.cz  <http://www.modxcms.cz>
+ * @author      Mihanik71 
  *
- * @see         http://codemirror.net/
+ * @see         https://github.com/Mihanik71/CodeMirror-MODx
  */
-
 global $content;
-
 $textarea_name = 'post';
-$object_name  = $content['name'];
-$obect_type  = substr($evt->name, 2, 1);
-
 $mode = 'htmlmixed';
-
+$lang = 'htmlmixed';
 /*
  * Default Plugin configuration
  */
@@ -31,17 +26,21 @@ $theme                  = (isset($theme)                    ? $theme            
 $indentUnit             = (isset($indentUnit)               ? $indentUnit               : 4);
 $tabSize                = (isset($tabSize)                  ? $tabSize                  : 4);
 $lineWrapping           = (isset($lineWrapping)             ? $lineWrapping             : false);
+$matchBrackets          = (isset($matchBrackets)            ? $matchBrackets            : false);
+$activeLine               = (isset($activeLine)             	? $activeLine            	: false);
+$emmet					= (($emmet == 'true')? '<script src="'.$_CM_URL.'cm/emmet-compressed.js"></script>' : "");
 /*
  * This plugin is only valid in "text" mode. So check for the current Editor
  */
 $prte   = (isset($_POST['which_editor']) ? $_POST['which_editor'] : '');
 $srte   = ($modx->config['use_editor'] ? $modx->config['which_editor'] : 'none');
 $xrte   = $content['richtext'];
-
+/*
+ * Switch event
+ */
 switch($modx->Event->name) {
     case 'OnTempFormRender'   :
         $object_name = $content['templatename'];
-
     case 'OnChunkFormRender'  :
         $rte   = ($prte ? $prte : 'none');
         break;
@@ -51,6 +50,24 @@ switch($modx->Event->name) {
         $object_name = $content['pagetitle'];
         $xrte  = (('htmlmixed' == $mode) ? $xrte : 0);
         $rte   = ($prte ? $prte : ($content['id'] ? ($xrte ? $srte : 'none') : $srte));
+		$contentType = $content['contentType'];
+		/*
+		* Switch contentType for doc
+		*/
+		switch($contentType){
+			case "text/css":
+				$mode = "text/css";
+				$lang = "css";
+			break;
+			case "text/javascript":
+				$mode = "text/javascript";
+				$lang = "javascript";
+			break;
+			case "application/json":
+				$mode = "application/json";
+				$lang = "javascript";
+			break;
+		}
         break;
 
     case 'OnSnipFormRender'   :
@@ -58,6 +75,7 @@ switch($modx->Event->name) {
     case 'OnModFormRender'    :
         $mode  = 'application/x-httpd-php-open';
         $rte   = ($prte ? $prte : 'none');
+		$lang = "php";
         break;
 
     case 'OnManagerPageRender':
@@ -70,136 +88,130 @@ switch($modx->Event->name) {
     default:
         $this->logEvent(1, 2, 'Undefined event : <b>'.$modx->Event->name.'</b> in <b>'.$this->Event->activePlugin.'</b> Plugin', 'CodeMirror Plugin : '.$modx->Event->name);
 }
-
-// object identifier for cursor position save in cookie
-$object_id = md5($obect_type.'-'.$object_name);
-$mgr_dir=MGR_DIR;
-
 if (('none' == $rte) && $mode) {
     $output = '';
-
-    if ($theme != 'default') {
-        $output .= <<< HEREDOC
-        <link rel="stylesheet" href="{$_CM_URL}cm/theme/{$theme}.css">
-HEREDOC;
-    }
-
     $output .= <<< HEREDOC
-    <script src="{$_CM_URL}cm/lib/codemirror.js"></script>
-    <link rel="stylesheet" href="{$_CM_URL}cm/lib/codemirror.css">
-
-    <script src="{$_CM_URL}cm/mode/xml/xml.js"></script>
-    <script src="{$_CM_URL}cm/mode/javascript/javascript.js"></script>
-    <script src="{$_CM_URL}cm/mode/css/css.js"></script>
-    <script src="{$_CM_URL}cm/mode/clike/clike.js"></script>
-    <script src="{$_CM_URL}cm/mode/htmlmixed/htmlmixed.js"></script>
-    <script src="{$_CM_URL}cm/mode/php/php.js"></script>
-
-    <link rel="stylesheet" href="{$_CM_URL}codemirror.plugin.css">
-    <script src="{$_CM_URL}codemirror.plugin.js"></script>
-
-    <script type="text/javascript">
-        /*
-         * Custom event handler
-         */
-        var myEventHandler = function(instance, event) {
-
-            // CTRL + S
-            if(event.ctrlKey == true && event.keyCode == 83) {
-                try {
-                    if(document.getElementById('Button1')) {
-                        document.getElementById('Button1').getElementsByTagName('a')[0].onclick();
-                    }
-                }
-                catch(event) {}
-
-                return event.stop();
-            }
-
-            // CTRL + E
-            if(event.ctrlKey == true && event.keyCode == 69) {
-                try {
-                    if(document.getElementById('Button1')) {
-                        document.getElementById('Button1').getElementsByTagName('select')[0].options[1].selected = true;
-                        document.getElementById('Button1').getElementsByTagName('a')[0].onclick();
-                    }
-                }
-                catch(event) {}
-
-                return event.stop();
-            }
-        }
-
-        /*
-         * Save of cursor position
-         */
-/*
-        var positionHolder = function() {
-            if(myCodeMirror) {
-                myCodeMirror.setLineClass(hlLine, null);
-                hlLine = myCodeMirror.setLineClass(myCodeMirror.getCursor().line, "activeline");
-
-                position = myCodeMirror.getCursor(false).line +'|'+ myCodeMirror.getCursor(false).ch;
-
-                setCookie('{$mgr_dir}','{$object_id}', position);
-            }
-        }
-*/
-
-        /*
-         * Main CodeMirror initialization
-         */
-        var config = {
-            mode: '{$mode}',
-            theme: '{$theme}',
-            indentUnit: {$indentUnit},
-            tabSize: '{$tabSize}',
-            lineNumbers: true,
-            matchBrackets: true,
-            onKeyEvent: myEventHandler,
-            lineWrapping: '{$lineWrapping}',
-            // onCursorActivity: positionHolder
-        };
-
-        var myCodeMirror = [];
-
-        var myTextArea = document.getElementsByName('$textarea_name')[0];
-        myCodeMirror.push(CodeMirror.fromTextArea(myTextArea, config));
-
-
-        if (document.getElementById('tv_body') !== null) {
-            var tv_textareas = $$('#tv_body textarea');
-            if (tv_textareas.length != 0) {
-                tv_textareas.each(function(el,t){
-                    myCodeMirror.push(CodeMirror.fromTextArea(el, config));
-                });
-            }
-        }
-
-        // check if tab was changed and then refresh codemirror instances
-        $$('.tab-row .tab').addEvents({
-            click: function() {
-                myCodeMirror.each(function(el) {
-                    el.refresh();
-                });
-            }
-        });
-
-        // var hlLine = myCodeMirror.setLineClass(0, "activeline");
-
-        /*
-         * Restore of cursor position
-         */
-/*
-        pos = getCookie('{$object_id}');
-        if(pos) {
-            pos = pos.split('|');
-
-            myCodeMirror.setCursor(pos[0] , pos[1]);
-        }
-*/
+	<link rel="stylesheet" href="{$_CM_URL}cm/lib/codemirror.css">
+	<link rel="stylesheet" href="{$_CM_URL}cm/theme/{$theme}.css">
+	<script src="{$_CM_URL}cm/lib/codemirror-compressed.js"></script>
+	<script src="{$_CM_URL}cm/addon-compressed.js"></script>
+	<script src="{$_CM_URL}cm/mode/{$lang}-compressed.js"></script>
+	{$emmet}
+	
+	<script type="text/javascript">
+		// Add mode MODx for syntax highlighting. Dfsed on $mode
+		CodeMirror.defineMode("MODx-{$mode}", function(config, parserConfig) {
+			var mustacheOverlay = {
+				token: function(stream, state) {
+					var ch;
+					if (stream.match("[[")) {
+						while ((ch = stream.next()) != null)
+							if (ch == "?" || (ch == "]"&& stream.next() == "]")) break;
+						return "modxSnippet";
+					}
+					if (stream.match("{{")) {
+						while ((ch = stream.next()) != null)
+							if (ch == "}" && stream.next() == "}") break;
+						stream.eat("}");
+						return "modxChunk";
+					}
+					if (stream.match("[*")) {
+						while ((ch = stream.next()) != null)
+							if (ch == "*" && stream.next() == "]") break;
+						stream.eat("]");
+						return "modxTv";
+					}
+					if (stream.match("[+")) {
+						while ((ch = stream.next()) != null)
+							if (ch == "+" && stream.next() == "]") break;
+						stream.eat("]");
+						return "modxPlaceholder";
+					}
+					if (stream.match("[!")) {
+						while ((ch = stream.next()) != null)
+							if (ch == "?" || (ch == "!"&& stream.next() == "]")) break;
+						return "modxSnippetNoCache";
+					}
+					if (stream.match("[(")) {
+						while ((ch = stream.next()) != null)
+							if (ch == ")" && stream.next() == "]") break;
+						stream.eat("]");
+						return "modxVariable";
+					}
+					if (stream.match("[~")) {
+						while ((ch = stream.next()) != null)
+							if (ch == "~" && stream.next() == "]") break;
+						stream.eat("]");
+						return "modxUrl";
+					}
+					if (stream.match("[^")) {
+						while ((ch = stream.next()) != null)
+							if (ch == "^" && stream.next() == "]") break;
+						stream.eat("]");
+						return "modxConfig";
+					}
+					if (stream.match("&")) {
+						while ((ch = stream.next()) != null)
+							if (ch == "=") break;
+						stream.eat("=");
+						return "attribute";
+					}
+					if (stream.match("!]")) {
+						return "modxSnippet";
+					}
+					if (stream.match("]]")) {
+						return "modxSnippetNoCache";
+					}
+					while (stream.next() != null && !stream.match("[[", false) && !stream.match("&", false) && !stream.match("{{", false) && !stream.match("[*", false) && !stream.match("[+", false) && !stream.match("[!", false) && !stream.match("[(", false) && !stream.match("[~", false) && !stream.match("[^", false) && !stream.match("!]", false) && !stream.match("]]", false)) {}
+					return null;
+				}
+			};
+			return CodeMirror.overlayMode(CodeMirror.getMode(config, parserConfig.backdrop || "{$mode}"), mustacheOverlay);
+		});
+		//Basic settings
+		var config = {
+			mode: 'MODx-{$mode}',
+			theme: '{$theme}',
+			indentUnit: {$indentUnit},
+			tabSize: {$tabSize},
+			lineNumbers: true,
+			matchBrackets: {$matchBrackets},
+			onKeyEvent: myEventHandler,
+			lineWrapping: {$lineWrapping},
+			gutters: ["CodeMirror-linenumbers", "breakpoints"],
+			styleActiveLine: {$activeLine},
+			indentWithTabs: true,
+			extraKeys:{
+				"Ctrl-Space": function(cm){
+					foldFunc_html(cm, cm.getCursor().line);
+				},
+				"F11": function(cm) {
+					setFullScreen(cm, !isFullScreen(cm));
+				},
+				"Esc": function(cm) {
+					if (isFullScreen(cm)) setFullScreen(cm, false);
+				}
+			}
+		};
+		var foldFunc_html = CodeMirror.newFoldFunction(CodeMirror.tagRangeFinder);
+		var myTextArea = document.getElementsByName('{$textarea_name}')[0];
+		var myCodeMirror = (CodeMirror.fromTextArea(myTextArea, config));
+		$$('.tab-row .tab').addEvents({
+			click: function() {
+				myCodeMirror.refresh();
+			}
+		});
+		myCodeMirror.on("gutterClick", function(cm, n) {
+			var info = cm.lineInfo(n);
+			cm.setGutterMarker(n, "breakpoints", info.gutterMarkers ? null : makeMarker());
+		});
+		function makeMarker() {
+			var marker = document.createElement("div");
+			marker.style.color = "#822";
+			marker.innerHTML = "●";
+			return marker;
+		}
     </script>
 HEREDOC;
-
     $modx->Event->output($output);
 }
